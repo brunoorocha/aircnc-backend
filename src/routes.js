08c7bbc7834847c35cddfb80f58e4f4f48f8293f
Bruno Rocha <mail.brunorocha@gmail.com>
2019-10-03 22:36:20 -0300

    Created POST /spot/:spot_id/bookings endpoint and implemented the booking storing

--- src/routes.js
+++ src/routes.js
@@ -10,12 +10,14 @@ const upload = multer(uploadConfig)
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
 const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to the AirCNC API ☕️' }))
 routes.post('/sessions', SessionController.store)
 
 routes.get('/spots', SpotController.index)
 routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 routes.get('/dashboard', DashboardController.show)
 
