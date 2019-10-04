0852526674b3a38e3f1419592ccede487f6c14f4
Bruno Rocha <mail.brunorocha@gmail.com>
2019-10-03 22:11:26 -0300

    Multer was installed and the file upload configured

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 const express = require('express')
+
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
 const routes = express.Router()
+const upload = multer(uploadConfig)
 
 // CONTROLLERS
 const SessionController = require('./controllers/SessionController')
@@ -9,6 +14,6 @@ routes.get('/', (req, res) => res.json({ message: 'Welcome to the AirCNC API ☕
 routes.post('/sessions', SessionController.store)
 
 routes.get('/spots', SpotController.index)
-routes.post('/spots', SpotController.store)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 
 module.exports = routes
